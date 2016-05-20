786c980f8862455ba2f6a1c7c5fcf74ff78639a5
Lawrence Hudson <lj1289@gmail.com>
2016-05-19 20:27:27 -0400

    First Commit

---
+++ week10Project/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week10Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********We Can Code IT resources*********\n\n");
+            List<string> menu = new List<string>();
+            menu.Add("1. View Student Account");
+            menu.Add("2. View Available Resources");
+            menu.Add("3. Check Out Item");
+            menu.Add("4. Check In Item");
+            menu.Add("6. Exit");
+
+            bool looptyLoop = true;
+            while (looptyLoop)
+            {
+                viewMenu(menu);
+                Console.Write("Please make a selection: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        break;
+                }              
+            }
+
+        }
+        static void viewMenu(List<string> menu)
+        {
+            foreach (string item in menu)
+            {
+                Console.WriteLine(item);
+            }
+            
+        } 
+         
+    }
+}
---
+++ week10Project/Resource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week10Project
+{
+    class Resource {// Base Class
+        //Fields & Properties
+
+    private string title;
+    private int resourceISBN;
+    private int pageNumbers;
+    //----------------------------------------------
+        public string Title
+    {
+        get { return this.title; }
+        set { this.title = value; }
+    }
+        public int ResourceISBN
+
+    {
+        get { return this.resourceISBN; }
+        set { this.resourceISBN = value;  }
+    }
+
+        public int PageNumbers
+    {
+        get { return this.pageNumbers; }
+        set { this.pageNumbers = value;  }
+    }
+    {
+        //Title(title of resource)
+        //ISBN(ISBN number of resource)
+        //Length(length of resource as # of pages)
+        //Status (available or checked out)
+
+      
+    }
+}
